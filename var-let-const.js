66e9f488e39a77cdab5e7a539c6e8533a56fc288
mufassir-mahmud <mufassirmahmud01821@gmail.com>
2023-11-24 18:43:15 +0600

    first commit

---
+++ var-let-const.js
@@ -0,0 +1,22 @@
+const money = 25;
+// money = 40;
+const rich = money + 40;
+console.log(rich);
+
+const numbers = [11,22,33,44,55,66];
+numbers[1] = 23;
+numbers.push(77,88,99)
+console.log(numbers);
+
+const student = {
+    name: 'Mufassir Mahmud',
+    roll: 484121,
+}
+student.name = 'Mahmud Mufassir'
+console.log(student);
+let sum = 0
+for( let i  = 1; i<= 10; i++){
+    const num = i;
+    sum = sum + num
+}
+console.log(sum);
