0eb3c60c5ce4a3137d73eca3d5f6cee55507af6a
mufassir-mahmud <mufassirmahmud01821@gmail.com>
2023-11-25 11:31:23 +0600

    template

--- template.js
+++ template.js
@@ -1,4 +1,18 @@
 const first = 'Jaan';
 const last = 'Pakhi';
-const name = first +" " + last;
+const greet = 'Potast potas'
+const name = first +" " + last + " " + greet;
 console.log(name);
+const a = 5;
+const b = 10;
+const result = 'The sum of ' + a+ " and " + b + ' is ' + (a+b);
+console.log(result);
+const math = `Thw result of ${a} and ${b} is ${a+b}`;
+console.log(math);
+const remail = `Hi jhon
+cena cena lage
+
+`;
+console.log(remail);
+
+const numbers = [11,22,33,44,55,66,77]
